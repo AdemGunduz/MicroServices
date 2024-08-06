57be17441716c82bebc2054921e9bfe9b92209d7
Adem GUNDUZ <adem.gunduz@beymen.com>
2024-08-06 18:00:27 +0300

    identityServer

--- Services/Catalog/Course.Services.Catalog/Program.cs
+++ Services/Catalog/Course.Services.Catalog/Program.cs
@@ -1,18 +1,33 @@
 using Course.Services.Catalog.Services;
 using Course.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Eriþim saðlanmýþ `Configuration` nesnesi
+var Configuration = builder.Configuration;
 
-builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = Configuration["IdentityServerURL"];
+    opt.Audience = "resource_catalog";
+    opt.RequireHttpsMetadata = false;
+});
+
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
+
 
 builder.Services.AddAutoMapper(typeof(Program));
 
@@ -32,6 +47,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
