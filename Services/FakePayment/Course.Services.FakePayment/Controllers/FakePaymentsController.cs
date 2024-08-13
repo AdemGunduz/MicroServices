fca13ed94a9e2126b148c0b726b129241b759a08
Adem GUNDUZ <adem.gunduz@beymen.com>
2024-08-13 11:32:21 +0300

    paymentServices

---
+++ Services/FakePayment/Course.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using Course.Shread.ControllerBases;
+using Course.Shread.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+
+        }
+    }
+}
